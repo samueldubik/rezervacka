6a181324b911e5b35b85f02814463022ff157a17
Samuel Dubik <samuel.dubik@gmail.com>
2023-08-09 01:28:36 +0200

    Fix 2

--- rezervacka/src/pages/api/fetch-floor-data.ts
+++ rezervacka/src/pages/api/fetch-floor-data.ts
@@ -6,7 +6,7 @@ const pool = new Pool({
   host: process.env.DB_HOST,
   database: process.env.DB_DATABASE,
   password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  port: Number(process.env.DB_PORT),
 });
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
