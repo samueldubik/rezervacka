bcab2a146b931a0a038f14d6f09117c5b6b14f38
Samuel Dubik <samuel.dubik@gmail.com>
2023-08-08 21:20:59 +0200

    Final commit?

--- rezervacka/src/app/page.tsx
+++ rezervacka/src/app/page.tsx
@@ -1,12 +1,41 @@
+'use client'
 import { db } from '@vercel/postgres'
 import  Main  from '../components/Main'
+import { useEffect, useState } from 'react';
+
 
 
 
 
 export default function Home() {
+
+  const [isSmallScreen, setIsSmallScreen] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsSmallScreen(window.innerWidth < 768);
+    };
+
+    handleResize();
+    console.log(window.innerWidth)
+
+    // Add a resize event listener to update the state
+    window.addEventListener('resize', handleResize);
+
+    // Clean up the event listener on component unmount
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
   
-  return (
-  <Main/>
-  )
+  if(!isSmallScreen)
+    return (
+      <Main/>
+    )
+  else{
+
+    return <div className=" w-screen h-screen px-10 flex flex-col justify-center">
+            <h1 className=" font-fira-sans text-center">Na registráciu je nutné použiť počítač. Ďakujem za pochopenie</h1>
+           </div>
+  }
 }
